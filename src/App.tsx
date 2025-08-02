1311a3e5cae6c9f2a71ce862950d7fbc5cb1a3dd
Dian Prasetyo <dprasmail@gmail.com>
2025-08-02 22:44:16 +0700

    feat: infinite scroll and pre commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,75 @@
-import { onValue, ref } from "firebase/database";
-import { useEffect, useState } from "react";
+import {
+  onValue,
+  startAfter,
+  limitToFirst,
+  ref,
+  query,
+  orderByKey,
+} from "firebase/database";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { db } from "./utils";
 import HotelCard from "./componnts/HotelCard";
 import type { IHotelData } from "./types";
 
+const limit = 5;
 function App() {
   const [hotels, setHotels] = useState<IHotelData[]>([]);
+  const [lastItemKey, setLastItemKey] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const loadingRef = useRef<HTMLDivElement | null>(null);
+
+  const loadHotels = useCallback(
+    (after?: string) => {
+      if (loading) return;
+
+      setLoading(true);
+      const queryConstraint = [limitToFirst(limit), orderByKey()];
+
+      if (after) {
+        queryConstraint.push(startAfter(after));
+      }
+
+      const hotelsQuery = query(ref(db, "hotels"), ...queryConstraint);
+      onValue(hotelsQuery, (snapshot) => {
+        if (snapshot.exists()) {
+          const lastKey = Object.keys(snapshot.val())[limit - 1];
+          const hotelsData = Object.values(snapshot.val()) as IHotelData[];
+          setLastItemKey(lastKey);
+          setHotels((prev) =>
+            after ? [...prev, ...hotelsData] : [...hotelsData],
+          );
+        }
+      });
+
+      setLoading(false);
+    },
+    [loading],
+  );
 
   useEffect(() => {
-    const query = ref(db, "hotels");
-    onValue(query, (snapshot) => {
-      if (snapshot.exists()) {
-        setHotels(Object.values(snapshot.val()));
+    loadHotels();
+  }, [loadHotels]);
+
+  useEffect(() => {
+    const callback = (entries: IntersectionObserverEntry[]) => {
+      if (entries[0].isIntersecting && lastItemKey && !loading) {
+        loadHotels(lastItemKey);
       }
-    });
-  }, []);
+    };
+
+    const option: IntersectionObserverInit = { threshold: 0.1 };
+    const observer = new IntersectionObserver(callback, option);
+
+    const loadingCurrent = loadingRef.current;
+    if (loadingCurrent) {
+      observer.observe(loadingCurrent);
+    }
+    return () => {
+      if (loadingCurrent) {
+        observer.unobserve(loadingCurrent);
+      }
+    };
+  }, [lastItemKey, loadHotels, loading]);
 
   return (
     <>
@@ -25,6 +80,14 @@ function App() {
             <HotelCard key={hotel.id} data={hotel} />
           ))}
         </section>
+        <div
+          ref={loadingRef}
+          className="w-full flex justify-center items-center"
+        >
+          {loading && (
+            <div className="w-6 h-6 mx-auto my-8 animate-spin rounded-full  border-2 border-gray-300 border-y-gray-900"></div>
+          )}
+        </div>
       </main>
     </>
   );
